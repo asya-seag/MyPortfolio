0dd6a3d8d579d1a5121fdafa0a73793688209e78
asya-seag <asyaseagrave@gmail.com>
2023-09-05 11:28:14 +0100

    Added Projects

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -9,7 +9,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import project1 from "../images/cfgsoftware.png";
 import project2 from "../images/project2.png";
 import project3 from "../images/project3.png";
-
+import project4 from "../images/project4.png";
+import project5 from "../images/project5.png";
 
 import "./ProjectsStyles.css";
 
@@ -35,6 +36,20 @@ const Projects = () => {
         "This is a group project for Mobile App development course, using React Native we created we added to our app Alcohol Units calculator, Login/Register functionality, search page with API connection and used hooks in a variety of page layouts.",
       image: project3,
       githubLink: "https://github.com/asya-seag/GroupProjectApp",
+    },
+    {
+      title: "Portfolio",
+            description:
+        "I used React to build this portfolio, it is my independent project and I have tried to implement components that i have not implemented before - top navigation bar, carousel.",
+      image: project4,
+      githubLink: "https://github.com/asya-seag/Portfolio",
+    },
+    {
+      title: "Learning",
+            description:
+        "As part of my proactive approach to learning, I've engaged in code-along projects where I follow expert instructors step-by-step. While these projects are guided, they've been invaluable in deepening my understanding of coding concepts, best practices, and real-world problem-solving.",
+      image: project5,
+      githubLink: "https://github.com/asya-seag/CSS_HTML_practice_project1",
     },
   ];
 
