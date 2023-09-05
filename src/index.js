1fe646cfd71394048a6f3817f29e231a1e6d99ff
asya-seag <asyaseagrave@gmail.com>
2023-09-05 15:10:52 +0100

    chng

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename="//MyPortfolio">
+  <BrowserRouter basename="//MyPortfolio/">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
