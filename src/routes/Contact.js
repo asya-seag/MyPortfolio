e71e251ffd42417fe28a29346080519d3fac7933
asya-seag <asyaseagrave@gmail.com>
2023-09-06 20:14:37 +0100

    chng

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -13,28 +13,30 @@ const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="main-contact">
-        <div className="contact-left">
-          <h1 className="contact-heading">Get in touch</h1>
-        </div>
+      <div className="contact-background">
+        <div className="main-contact">
+          <div className="contact-left">
+            <h1 className="contact-heading">Get in touch</h1>
+          </div>
 
-        <div className="contact-right">
-          <button className="get-in-touch-button" onClick={handleEmailClick}>
-            E-mail: asyaseagrave@gmail.com
-          </button>
-          <p className="mobile">Mobile: 07770672445</p>
+          <div className="contact-right">
+            <button className="get-in-touch-button" onClick={handleEmailClick}>
+              E-mail: asyaseagrave@gmail.com
+            </button>
+            <p className="mobile">Mobile: 07770672445</p>
 
-          <a
-            href="https://www.linkedin.com/in/asya-seagrave/"
-            className="contact-link"
-          >
-            <LinkedinIcon className="linkedin-icon" />
-            My LinkedIn profile
-          </a>
-          <a href="https://github.com/asya-seag" className="contact-link">
-            <GitHubIcon className="github-icon" />
-            My GitHub page
-          </a>
+            <a
+              href="https://www.linkedin.com/in/asya-seagrave/"
+              className="contact-link"
+            >
+              <LinkedinIcon className="linkedin-icon" />
+              My LinkedIn profile
+            </a>
+            <a href="https://github.com/asya-seag" className="contact-link">
+              <GitHubIcon className="github-icon" />
+              My GitHub page
+            </a>
+          </div>
         </div>
       </div>
     </div>
